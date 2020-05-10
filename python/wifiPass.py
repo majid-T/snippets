d36d7ccb1976e2669bb53dac68eb35066b69406c
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-10 05:07:23 +0430

    extracting password from report of each profile

--- python/wifiPass.py
+++ python/wifiPass.py
@@ -5,3 +5,13 @@ wifiNames = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
 wifiNames = wifiNames.decode('utf-8').split('\n')
 wifiNames = [i.split(':')[1][1:-1]
              for i in wifiNames if 'All User Profile' in i]
+for name in wifiNames:
+    password = subprocess.check_output(
+        ['netsh', 'wlan', 'show', 'profile', name, 'KEY=CLEAR']).decode('utf-8').split('\n')
+    password = [line.split(':')[1][1:-1]
+                for line in password if 'Key Content' in line]
+    try:
+        print('{:<30}| {:<}'.format(name, password[0]))
+    except IndexError:
+        print('{:<30}| {:<}'.format(
+            name, 'No PASSWORD - other authentication protocol might been used'))
