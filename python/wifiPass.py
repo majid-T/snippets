25e76a265a0fae8f94bbd3cb5491a33e5e72dbeb
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-10 05:10:18 +0430

    Code done- comments added

--- python/wifiPass.py
+++ python/wifiPass.py
@@ -1,10 +1,14 @@
-# snippet for see the saved passwords around
+# snippet to extract passwords for saved wifi profiles
 import subprocess
 
+
+# Getting saved wifi profiles using cmd command netsh wlan show profiles
 wifiNames = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
 wifiNames = wifiNames.decode('utf-8').split('\n')
 wifiNames = [i.split(':')[1][1:-1]
              for i in wifiNames if 'All User Profile' in i]
+
+# Looping  in names and using cmd command netsh wlan show profile WIFI_NAME with KEY=CLEAR switch and print result
 for name in wifiNames:
     password = subprocess.check_output(
         ['netsh', 'wlan', 'show', 'profile', name, 'KEY=CLEAR']).decode('utf-8').split('\n')
