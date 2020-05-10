ce16b5531846258cbe6fa9663a69dea6e3f27212
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-10 05:32:13 +0430

    ping some ip script completed as a function

--- python/pingScript.py
+++ python/pingScript.py
@@ -2,12 +2,15 @@ import subprocess
 
 
 def pingSomeIp(destIp):
+    timeResult = []
     pingResult = subprocess.check_output(['ping', destIp]).decode('utf-8')
-    pingResultArr = pingResult.split('\n')
-    return pingResultArr[2:6]
+    pingResultArr = pingResult.split('\n')[2:6]
+    for i in pingResultArr:
+        timeResult.append(i.split(' ')[4][5:-2])
+    return timeResult
 
+# Uncomment to test script
+# res = pingSomeIp('8.8.8.8')
 
-res = pingSomeIp('8.8.8.8')
-
-for item in res:
-    print(item)
+# for item in res:
+#     print(item)
