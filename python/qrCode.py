e043c13f9dd058392171a86ea6664a33804d08d4
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-24 22:43:47 +0430

    Fixed file Destination bug

--- python/qrCode.py
+++ python/qrCode.py
@@ -5,14 +5,14 @@ from PIL import Image, ImageTk
 
 win = Tk()
 win.title('QR Code Genereator')
-win.config(background='#ffccee')
+win.config(background='#ffcc00')
 
 
 def generateQrCode():
     txt = entry1.get()
     qrCode = pyqrcode.create(txt)
     destFile = 'theQrCode'
-    save_path = r'C:\Users\majid\Desktop'
+    save_path = r'C:\Users\majid\Desktop\ '
     name = save_path+destFile+'.png'
 
     qrCode.png(name, scale=10)
@@ -24,7 +24,7 @@ def generateQrCode():
     win.imagelabel.photo = image
 
 
-text = ttk.Label(win, text='Enter text for QR Code')
+text = ttk.Label(win, text='Enter text')
 text.grid(row=0, column=0, padx=3, pady=3)
 
 entry1 = ttk.Entry(win, width=40)
@@ -36,7 +36,7 @@ button.grid(row=0, column=2, padx=3, pady=3)
 show_qr = ttk.Label(win, text='Code')
 show_qr.grid(row=1, column=0, padx=3, pady=3)
 
-win.imagelabel = ttk.Label(win, background='#000000')
+win.imagelabel = ttk.Label(win, background='#ffcc00')
 win.imagelabel.grid(row=2, column=0, padx=3, pady=3, columnspan=3)
 
 win.mainloop()
