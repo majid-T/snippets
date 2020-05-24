22f9db16da92501b9abb836c90daac32cbfa329c
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-24 18:15:47 +0430

    Added if to control no file selected

--- python/csvLoader.py
+++ python/csvLoader.py
@@ -10,17 +10,21 @@ win.title('CSV Loader')
 def open():
     file = askopenfile(mode='r', filetypes=[('CSV File', '*.csv')])
 
-    content = pd.read_csv(file)
+    content = 'No file Selected!'
+    if(file):
+        content = ''
+        content = pd.read_csv(file)
+
     data.insert(tk.END, content)
 
 
-label = tk.Label(win, text='Select File')
+label = tk.Label(win, text='Select a CSV File to be loaded below')
 label.grid(row=0, column=0, padx=8, pady=8)
 
 button = tk.Button(win, text='Load File', command=open)
 button.grid(row=0, column=1, padx=8, pady=8)
 
-data = tk.Text(win, height=10, width=30)
+data = tk.Text(win, height=20, width=60)
 data.grid(row=3, column=0, columnspan=2)
 
 win.mainloop()
