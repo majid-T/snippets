429c9447b7cfcbd9b1ce7c51721a61298d53e991
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-08 21:57:40 +0430

    Added comments

--- python/ceasarCipher.py
+++ python/ceasarCipher.py
@@ -1,4 +1,6 @@
 # Simplified Ceasar Cipher example works on alphabetic charachtors only.
+# Numbers provide a small pre-image since we are ciphering on each word. So numbers are not
+# accepted and instead of 1 you should type one to increese security
 
 # All alphabet chars in a list
 chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
@@ -6,6 +8,7 @@ chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', '
          'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
+# Function to cipher a charcahter by provided shift
 def getCipherChar(inCh, shift):
     # For space only return space
     if inCh == ' ':
@@ -17,10 +20,14 @@ def getCipherChar(inCh, shift):
     # shift the index
     newIndex = index+shift
 
+    # Check if new index is bigger than lenght of list -1 to avoid out of range index,
+    # if it is deduct lenght to cycle from begining
     if(newIndex > len(chars)-1):
         newIndex -= len(chars)
 
     return chars[newIndex]
+
+# Function to iterate n a phrase and chipher charachters
 
 
 def cipherPhrase(phrase, shift):
@@ -29,4 +36,3 @@ def cipherPhrase(phrase, shift):
         result += getCipherChar(ch, shift)
 
     return result
-
