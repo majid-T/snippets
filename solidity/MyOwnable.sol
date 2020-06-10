e7822403f0aec4946cfaf4eeecb1b9a81650cb4a
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-10 22:19:17 +0430

    Added comment to the code

--- solidity/MyOwnable.sol
+++ solidity/MyOwnable.sol
@@ -2,14 +2,20 @@ pragma solidity ^0.6.0;
 
 
 contract MyOwnable {
+    // State variable address which will be set by constructor upon deployment
+    // will be used later to determine the owner of the contract
     address owner;
 
+    // An event to keep track every time this contract changes ownership
     event CahngeOwnership(address oldOwner, address newOwner, uint256 time);
 
+    // The constructor which will set the deployer of this contract as the owner
     constructor() public {
         owner = msg.sender;
     }
 
+    // Change ownership function which will change the owner of contract.
+    // This contract has a require to make sure only owner can call it or revert
     function changeOwner(address newOwner) public returns (bool) {
         require(msg.sender == owner, "Only owner can call this function");
         owner = newOwner;
