b18c95927bf289c9e396210fac0d0b5bcf901ea2
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-10-16 02:36:49 -0400

    Added email validation

--- css/html-inputs/script-subscribe.js
+++ css/html-inputs/script-subscribe.js
@@ -1,12 +1,18 @@
 var inputActive = false;
+var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 var subFunct = function () {
     if (!inputActive) {
         inputActive = true
     } else {
-        var emailinputField = document.getElementById('userEmail');
-        window.alert(`${emailinputField.value} was added to our mailing list`);
-        emailinputField.value = '';
+        var emailInput = document.getElementById('userEmail');
+        var userEmail = emailInput.value;
+        if (emailRegex.test(userEmail)) {
+            window.alert(`${emailInput.value} was added to our mailing list`);
+        } else {
+            window.alert(`${emailInput.value} is not a valid email`);
+        }
+        emailInput.value = '';
         inputActive = false
     }
 }
