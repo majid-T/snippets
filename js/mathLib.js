1b600678393254f89015b4de0cc1d84fff94498a
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-12-11 18:24:23 -0500

    Added mathLib MMM function

---
+++ js/mathLib.js
@@ -0,0 +1,37 @@
+//Get Median, MEan and Mode in array of numbers 
+//Median: The middle number; found by ordering all data points and picking out the one in the middle
+// (or if there are two middle numbers, taking the mean of those two numbers).
+//Mean: The "average" number; found by adding all data points and dividing by the number of data points.
+//Mode: The most frequent number—that is, the number that occurs the highest number of times.
+const findMMM = (_arrNum) => {
+    console.log(`Input array is ${_arrNum}`)
+    const result = { median: null, mean: null, mode: null }
+
+    let tmpArr = _arrNum;
+    tmpArr.sort((a, b) => a - b)
+    result.median = tmpArr.length % 2 === 0 ?
+        ((tmpArr[Math.floor(tmpArr.length / 2)] + tmpArr[Math.floor(tmpArr.length / 2) - 1])) / 2
+        :
+        (tmpArr[Math.floor(tmpArr.length / 2)])
+
+    result.mean = parseFloat(tmpArr.reduce((acc, curV) => acc + curV) / tmpArr.length).toFixed(2);
+
+    const uVals = [...new Set(tmpArr)]
+
+    let v = tmpArr[0];
+    let cnt = 0;
+
+    uVals.forEach((e) => {
+        let countArr = tmpArr.filter(num => num === e);
+        if (countArr.length > cnt) {
+            v = e;
+            cnt = countArr.length
+        }
+    })
+
+    result.mode = v
+
+    console.log(result)
+
+
+}
