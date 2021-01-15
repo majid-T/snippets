e864a8287f0faa3a98b86171dca07d6d4ad3637b
Majid Shockoohi <majid.shockoohi@gmail.com>
2021-01-14 20:04:45 -0500

    Added two button for example of changing state from user action

--- react/redux-boiler/src/components/Counter.js
+++ react/redux-boiler/src/components/Counter.js
@@ -1,17 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { addOne, minusOne } from "../actions/counter";
 
-const Counter = ({ value }) => {
-  return <p>{value}</p>;
+const Counter = ({ value, addOne, minusOne }) => {
+  return (
+    <>
+      <p>{value}</p>
+      <div>
+        <span className="btn" onClick={addOne}>
+          +
+        </span>
+        <span className="btn" onClick={minusOne}>
+          -
+        </span>
+      </div>
+    </>
+  );
 };
 
 Counter.propTypes = {
   value: PropTypes.number,
+  addOne: PropTypes.func,
+  minusOne: PropTypes.func,
 };
 
 const mapStateToProps = (state) => ({
   value: state.counter.value,
 });
 
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, { addOne, minusOne })(Counter);
