2bc77b03f66ab16821de0a6c1fd544fd5154e020
Majid Shockoohi <majid.shockoohi@gmail.com>
2021-01-18 19:59:37 -0500

    Added some notes

--- serach-algorithms/js-binarySearch.js
+++ serach-algorithms/js-binarySearch.js
@@ -1,15 +1,28 @@
-const binarySearch = (array, search) => {
-  let start = 0;
+/*
+This a binary search representation in java script with Time complexity O (log n) on search (GET)
+Note1: binary search works on sorted lists
+Note2: this will by default work on array on numbers
 
+*/
+
+const binarySearch = (array, search) => {
+  //setting start to element 0 and end to ast element of array
+  let start = 0;
   let end = array.length - 1;
 
+  // loop till end or start are not equal
   while (start <= end) {
+    //get the middle element index to do comparison on it
     let mid = Math.floor((start + end) / 2);
 
+    // If it is found retrun the element
     if (array[mid] === search) {
       return array[mid];
     }
 
+    // if middle element is less than search element we need to search the upper half of the array
+    // then set start to one index after middle index 
+    // if it is less than middle element then we need to search the lower half of the array
     if (array[mid] < search) {
       start = mid + 1;
     } else {
@@ -17,6 +30,7 @@ const binarySearch = (array, search) => {
     }
   }
 
+  // if not found return -1
   return -1;
 };
 
