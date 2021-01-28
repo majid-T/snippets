ddb871771063e20d54351c04854275d5f2b4a78b
Majid Shockoohi <majid.shockoohi@gmail.com>
2021-01-27 20:48:02 -0500

    Added components and pages to app.js

--- react/router-dom-bolier/src/App.js
+++ react/router-dom-bolier/src/App.js
@@ -1,9 +1,35 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ContactUs from "./pages/ContactUs";
+import Products from "./pages/Products";
+import Services from "./pages/Services";
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>{" "}
+        <Route exact path="/services">
+          <Services />
+        </Route>{" "}
+        <Route exact path="/products">
+          <Products />
+        </Route>{" "}
+        <Route exact path="/contact-us">
+          <Services />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
