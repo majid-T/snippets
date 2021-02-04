be81487286a554df0640de82cfc539defb4682f2
Majid Shockoohi <majid.shockoohi@gmail.com>
2021-02-04 04:17:10 -0500

    Added some comments

--- python/csvReader.py
+++ python/csvReader.py
@@ -1,10 +1,12 @@
 import csv
 
+# assume file name is file.csv --> this can later come from user
 with open('file.csv') as dataFile:
     reader = csv.DictReader(dataFile)
     
     print('='*100)
 
+    # header names are known so we can format based on what we know
     for row in reader:
         print('-+'*48)
         print('Name: ' + row['lastname'] + ", " + row['name'])
