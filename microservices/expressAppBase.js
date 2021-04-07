cf98f6859d9ccf05b5e6cfa5deabb046564fd20a
Majid Shockoohi <majid.shockoohi@gmail.com>
2021-04-07 17:53:28 -0400

    Base express Class

---
+++ microservices/expressAppBase.js
@@ -0,0 +1,35 @@
+const express = require("express"),
+  bodyParser = require("body-parser");
+
+/**
+ * Class for a base express app
+ * @class
+ * @classdesc This is a base Class for Express Application in node.
+ *
+ */
+class ExpressApp {
+  /**
+   * Class constructor
+   * @param {object} conntext
+   * @return {ExpressApp}
+   */
+  constructor(conntext) {
+    //Set options if present on context
+    this.options = conntext.options || {};
+
+    const app = express();
+    const router = express.Router();
+
+    this.express = express;
+    this.app = app;
+    this.router = router;
+
+    this.defaultRoute = this.options.defaultRoute || "/";
+
+    app.use(
+      bodyParser.urlencoded({
+        extended: true,
+      })
+    );
+  }
+}
